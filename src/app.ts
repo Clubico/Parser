7de746c203ad096bcfe7e8afbab69d88dfb8a7bc
Pawel1894 <pawepohl@gmail.com>
2023-11-12 00:52:25 +0100

    Just testing

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import { Converter } from './string-converter/index.ts';
 
 function main() {  
   // For testing purposes
+  // testing PR
   const input = '{"name": "John"}';
   const test = Converter.convert(input, "JSON").toJS();
   console.log(test);
